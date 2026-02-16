71ac75085a0bdf41fcc2c2d3f857656b9ad27033
Lukas <lukasnoller@gmail.com>
2026-02-16 19:04:18 +0100

    Finish paddle drawingi refactoring, begin ball drawing

--- internal/pong/draw.go
+++ internal/pong/draw.go
@@ -1,16 +1,54 @@
 package pong
 
-func (p Pong) drawPaddle(grid [][]string) [][]string {
-	for j := p.PaddleCoordinates[1]; j < p.PaddleCoordinates[1]+p.PaddleHeight; j++ {
+func (p Pong) drawTopHalf(grid [][]string, j int) [][]string {
 
-		for i := p.PaddleCoordinates[0]; i < p.PaddleCoordinates[0]+p.PaddleWidth; i++ {
-			if i == p.PaddleCoordinates[0] || i == p.PaddleCoordinates[0]+p.PaddleWidth-1 {
-				grid[j][i] = "|"
-			} else {
-				grid[j][i] = "-"
-			}
+	for i := p.PaddleCoordinates[0]; i < p.PaddleCoordinates[0]+p.PaddleWidth; i++ {
+		switch i {
+		case p.PaddleCoordinates[0]:
+
+			grid[j][i] = "╭"
+		case p.PaddleCoordinates[0] + p.PaddleWidth - 1:
+			grid[j][i] = "╮"
+
+		default:
+			grid[j][i] = "-"
 		}
 	}
+	return grid
+}
+func (p Pong) drawBottomHalf(grid [][]string, j int) [][]string {
+
+	for i := p.PaddleCoordinates[0]; i < p.PaddleCoordinates[0]+p.PaddleWidth; i++ {
+		switch i {
+		case p.PaddleCoordinates[0]:
+
+			grid[j][i] = "╰"
+		case p.PaddleCoordinates[0] + p.PaddleWidth - 1:
+			grid[j][i] = "╯"
+
+		default:
+			grid[j][i] = "-"
+		}
+	}
+	return grid
+}
+func (p Pong) drawMiddle(grid [][]string, j int) [][]string {
+
+	grid[j][p.PaddleCoordinates[0]] = "|"
+	grid[j][p.PaddleCoordinates[0]+p.PaddleWidth-1] = "|"
+	return grid
+}
+func (p Pong) drawPaddle(grid [][]string) [][]string {
+	grid = p.drawTopHalf(grid, p.PaddleCoordinates[1])
+	for j := p.PaddleCoordinates[1] + 1; j < p.PaddleCoordinates[1]+p.PaddleHeight-1; j++ {
+		p.drawMiddle(grid, j)
+	}
+	grid = p.drawBottomHalf(grid, p.PaddleCoordinates[1]+p.PaddleHeight-1)
+
+	return grid
+}
+func (p Pong) drawBall(grid [][]string) [][]string {
+	grid[p.BallCoordinates[1]][p.BallCoordinates[0]] = "X"
 	return grid
 }
 func (p Pong) drawBoard() string {
